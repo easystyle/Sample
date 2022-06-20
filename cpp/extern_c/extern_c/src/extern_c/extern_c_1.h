6e178617e932d8eadd35be1b58264d5f84bce980
easystyle <keepout_easy@hotmail.com>
2022-06-21 05:57:44 +0900

    Delete cpp directory

--- cpp/extern_c/extern_c/src/extern_c/extern_c_1.h
+++
@@ -1,2 +0,0 @@
-/* 関数のプロトタイプ宣言 */
-void extern_c_1_func(void); /* extern_c_1.cの関数extern_c_1_funcの宣言. */
--- cpp/extern_c/extern_c/src/extern_c/extern_c_2.h
+++
@@ -1,2 +0,0 @@
-/* 関数のプロトタイプ宣言 */
-void extern_c_2_func(void); /* extern_c_2.cppの関数extern_c_2_funcの宣言. */
