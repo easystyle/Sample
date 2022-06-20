69c6cd77d6f41fd9a5fcd6e78dda585adb1bcff4
easystyle <keepout_easy@hotmail.com>
2022-06-21 06:02:43 +0900

    Delete google/oauth2 directory

--- google/oauth2/token/src/oauth2/oauth2/credentials.h
+++
@@ -1,3 +0,0 @@
-#define CLIENT_ID _T("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-#define CLIENT_SECRET _T("xxxxxxxxxxxxxxxxxxxxxxxx")
-#define REDIRECT_URI _T("xxxxxxxxxxxxxxxxxxxxxxxxx")
